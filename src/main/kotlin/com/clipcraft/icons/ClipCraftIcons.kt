ede60fea15f7a278ed01b09b391eb6f56836d3e9
Anthony Tabano <tabano86@gmail.com>
2025-02-11 00:53:28 -0500

    Add new 32px ClipCraft SVG icon
    Introduced a new SVG icon for ClipCraft in the resources folder. This addition supports consistent visual representation across the application. It uses a 32px size to align with UI design requirements.

--- src/main/kotlin/com/clipcraft/icons/ClipCraftIcons.kt
+++ src/main/kotlin/com/clipcraft/icons/ClipCraftIcons.kt
@@ -4,6 +4,23 @@ import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
 object ClipCraftIcons {
+
     @JvmField
-    val CLIPCRAFT: Icon = IconLoader.getIcon("/icons/clipcraft.svg", ClipCraftIcons::class.java)
+    val CLIPCRAFT_16: Icon =
+        IconLoader.getIcon("/icons/clipcraft_16.svg", ClipCraftIcons::class.java)
+
+    @JvmField
+    val CLIPCRAFT_24: Icon =
+        IconLoader.getIcon("/icons/clipcraft_24.svg", ClipCraftIcons::class.java)
+
+    @JvmField
+    val CLIPCRAFT_32: Icon =
+        IconLoader.getIcon("/icons/clipcraft_32.svg", ClipCraftIcons::class.java)
+
+    /**
+     * For convenience, we can define a default icon to use if no size specified.
+     * Usually 16×16 is standard for toolbar actions.
+     */
+    @JvmField
+    val CLIPCRAFT_DEFAULT: Icon = CLIPCRAFT_16
 }

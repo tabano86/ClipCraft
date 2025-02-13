3321ba47b799e674987bb52fb483b4bda5133a03
Anthony Tabano <tabano86@gmail.com>
2025-02-12 20:31:41 -0500

    fix: remove unnecessary blank line in code
    Removed an extraneous blank line in `ClipCraftGitIntegration.kt` to improve code formatting and readability. This change has no functional impact.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,15 +2,13 @@ plugins {
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.20"
 }
 
+
 gitHooks {
+    preCommit {
+        tasks("spotlessApply")
+    }
     commitMsg {
         conventionalCommits { }
     }
-    preCommit {
-        from {
-            "gradlew spotlessApply"
-        }
-    }
-
     createHooks()
 }

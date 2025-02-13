e5075dda48457229e05b351ec54656ff9a5d832e
Anthony Tabano <tabano86@gmail.com>
2025-02-12 19:51:35 -0500

    feat: update plugin compatibility and build settings
    Enabled `updateSinceUntilBuild` in Gradle and updated `since-build` and `until-build` versions in plugin.xml for better plugin compatibility with newer IntelliJ IDEA versions.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,17 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-        maven("https://cache-redirector.jetbrains.com/plugins.gradle.org/m2/")
-    }
-}
-rootProject.name = "ClipCraft"
-
-// Apply the pre-commit git hooks plugin without immediately applying its functionality.
 plugins {
-    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.20" apply false
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.20"
 }
 
-if (gradle.startParameter.projectProperties.containsKey("gitHooks")) {
-    gitHooks {
-        commitMsg {
-            conventionalCommits {
-                defaultTypes()
-            }
+gitHooks {
+    preCommit {
+        appendScript {
+            "spotlessApply"
         }
-        createHooks()
     }
+
+    createHooks()
+}
+
+gitHooks.preCommit {
+    tasks("ktlintCheck")
 }
